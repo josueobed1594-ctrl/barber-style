a17e3d53e55ff5c3e8918809533909eb82389c5c
Josue Obed <josueobed13@gmail.com>
2026-06-14 12:35:21 -0500

    fix gulp production

--- config/db.js
+++ config/db.js
@@ -1,20 +1,27 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
 });
 
 connection.connect((error) => {
     if (error) {
-        console.error('❌ Error al conectar con MySQL:', error);
+        console.error('❌ Error MySQL:', error);
         return;
     }
 
-    console.log('✅ Conexión exitosa a MySQL');
+    console.log(
+        isProduction
+            ? '🚀 Conectado a Railway (PROD)'
+            : '🖥 Conectado a MySQL Local'
+    );
 });
 
 module.exports = connection;
